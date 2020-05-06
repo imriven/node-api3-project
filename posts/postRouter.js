5fb5680eaf709330005d2674a46e0150ec0b51a1
Christina Harris <christinathedesigner@outlook.com>
2020-05-06 16:08:37 -0700

    working on stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,66 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+const db = require("./postDb");
+const userdb = require("../users/userDb")
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "error connecting to database" });
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  res.status(200).json(req.post)
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(Number(req.params.id))
+  .then(result => {
+    if (result === 1) {
+      res.status(204).send()
+    } else {
+      res.status(500).json({ error: "error deleting post" })
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  db.update(Number(req.params.id), req.body)
+  .then(result => {
+    if (result === 1) {
+      res.status(204).send()
+    } else {
+      res.status(500).json({ error: "error updating record" })
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  db.getById(req.id)
+  .then((result) => {
+    if (result) {
+      req.post = result;
+      next();
+    } else {
+      res.status(400).json({ message: "invalid post id" });
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,66 +1,125 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
-const db = require("./userDb")
-router.post('/', (req, res) => {
-  // do your magic!
+const db = require("./userDb");
+const postdb = require("../posts/postDb")
+
+
+router.post("/", validateUser, (req, res) => {
+  db.insert(req.body)
+    .then((result) => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "error connecting to database" });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
+  const p=req.body
+  p.user_id = Number(req.params.id)
+  postdb.insert(p)
+  .then(result => {
+    res.status(201).send()
+  })
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   db.get()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "error connecting to database" });
+    });
+});
+
+router.get("/:id", convertId, validateUserId, (req, res) => {
+  res.status(200).json(req.user);
+});
+
+router.get("/:id/posts", validateUserId, (req, res) => {
+  db.getUserPosts(Number(req.params.id))
   .then(result => {
     res.status(200).json(result)
   })
-  .catch(err => {
-    res.status(500).json({error: "error connecting to database"})
-  })
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
 });
 
-router.get('/:id', (req, res) => {
-  db.getById(Number(req.params.id))
+router.delete("/:id", validateUserId, (req, res) => {
+  db.remove(Number(req.params.id))
   .then(result => {
-    res.status(200).json(result)
+    if (result === 1) {
+      res.status(204).send()
+    } else {
+      res.status(500).json({ error: "error deleting record" })
+    }
   })
-  .catch(err => {
-    res.status(500).json({error: "error connecting to database"})
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
+});
+
+router.put("/:id", validateUserId, (req, res) => {
+  db.update(Number(req.params.id), req.body)
+  .then(result => {
+    if (result === 1) {
+      res.status(204).send()
+    } else {
+      res.status(500).json({ error: "error updating record" })
+    }
   })
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+  .catch((err) => {
+    res.status(500).json({ error: "error connecting to database" });
+  });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  db.getById(Number(req.params.id))
-  .then(result => {
-  req.user = result
-  next();
-  })
-  .catch(err => {
-    res.status(400).json({ message: "invalid user id" })
-  })
+  db.getById(req.id)
+    .then((result) => {
+      if (result) {
+        req.user = result;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid user id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "error connecting to database" });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (JSON.stringify(req.body) === '{}') {
+  return res.status(400).json({ message: "missing user data" })
+  }
+  if (!req.body.name) {
+    return res.status(400).json({ message: "missing required name field" })
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (JSON.stringify(req.body) === '{}') {
+    return res.status(400).json({ message: "missing post data" })
+    }
+    if (!req.body.text) {
+      return res.status(400).json({ message: "missing required text field" })
+    }
+    next();
+}
+
+function convertId(req, res, next) {
+  req.id = Number(req.params.id)
+  next()
 }
 
 module.exports = router;
+
+

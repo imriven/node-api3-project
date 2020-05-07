52cbeeeb8826c65abdeac4bdea0444595475f47e
Christina Harris <christinathedesigner@outlook.com>
2020-05-07 07:58:23 -0700

    heroku setup

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // code away!
 const server = require("./server")
 
-server.listen(8000, () => console.log('API running on port 8000'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+
+server.listen(port, () => console.log('API running on port 8000'));
